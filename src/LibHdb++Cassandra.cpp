fb1eaa14499d4afe1ff514946052e6eaf11d454b
Stuart James <stuart.james@esrf.fr>
2017-06-15 16:27:44 +0200

    Cut and paste error on the license

--- src/LibHdb++Cassandra.cpp
+++ src/LibHdb++Cassandra.cpp
@@ -1,16 +1,21 @@
-//=============================================================================
-//
-// file :        LibHdb++Cassandra.cpp
-//
-// description : Source file for the LibHdb++Cassandra library.
-//
-// Author: Reynald Bourtembourg
-//
-// $Revision: $
-//
-// $Log: $
-//
-//=============================================================================
+/* Copyright (C) : 2017
+   European Synchrotron Radiation Facility
+   BP 220, Grenoble 38043, FRANCE
+
+   This file is part of libhdb++cassandra.
+
+   libhdb++cassandra is free software: you can redistribute it and/or modify
+   it under the terms of the Lesser GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   libhdb++cassandra is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Lesser
+   GNU General Public License for more details.
+
+   You should have received a copy of the Lesser GNU General Public License
+   along with Foobar.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include "LibHdb++Cassandra.h"
 
