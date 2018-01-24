3376a968cfa537469989bd01716e5dc95f404f1a
Stuart James <stuart.james@esrf.fr>
2018-01-24 11:57:28 +0100

    Fixes to build after merge. Updated to Catch2 for testing

--- test/TangoEventDataBinderTests.cpp
+++ test/TangoEventDataBinderTests.cpp
@@ -14,7 +14,6 @@
    along with libhdb++cassandra.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include "TangoEventDataBinder.h"
-#include "SimpleCassandraConnection.h"
 
 using namespace std;
 using namespace HDBPP;
